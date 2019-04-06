0425412228bba2bbc98f1d2731dfa33d41685e10
yuseong park <>
2019-04-06 14:41:08 +0900

    scss 적용

---
+++ src/pages/heroes/Heroes.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import * as axios from 'axios/index';
+import styles from './Heroes.module.scss';
+
+export class Heroes extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            heroes: []
+        }
+    }
+
+    render() {
+
+        return (
+            <ul className={styles["img-box"]}>
+                {this.state.heroes.map(hero => (
+
+                    <li key={hero.hero_id}>
+
+                        <img src={hero.photo} alt={hero.name} className={styles.img}/>
+                            <span>{hero.name}</span>
+                    </li>
+                ))}
+            </ul>
+        )
+    }
+
+    componentDidMount() {
+        this.getHeroes();
+    }
+
+    getHeroes = async() => {
+
+        const res = await axios.get('http://eastflag.co.kr:8080/api/heroes');
+        this.setState({heroes: res.data});
+        // let response = axios.get('http://eastflag.co.kr:8080/api/heroes').then(res => console.log(res.data));
+    }
+
+}
